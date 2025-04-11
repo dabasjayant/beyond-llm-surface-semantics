dd8b39fc365daacc7d3dd8677dd6dbe46f7789bb
Jayant Dabas <jayantd1112@gmail.com>
2025-04-11 18:05:15 -0400

    add scenario builder module

---
+++ src/builder/prompt_generator.py
@@ -0,0 +1,38 @@
+class PromptGenerator:
+  def __init__(self):
+    self.history = ''
+
+  def create_history(self, prompt, output):
+    self.history = f'Our conversation history: \n\n[User]: \n{prompt} \n\n[Your answer]: \n{output} \n\n\n'
+
+  def update_history(self, prompt, output):
+    self.history = self.history + f'[User]: \n{prompt} \n\n[Your answer]: \n{output} \n\n\n'
+
+  def get_history(self):
+    return self.history
+  
+  def delete_history(self):
+    self.history = ''
+
+  def generate_prompt(self, data, is_update=False):
+    prompt = 'Updated information:\n\n' if is_update else 'Answer the following question based on the given facts, rules, and preferences. Provide a true or false answer (binary cross-entropy).\n\n'
+
+    # Add facts to the prompt
+    prompt += 'Facts:\n'
+    for fact in data['facts']:
+      prompt += f'- {fact}\n'
+
+    # Add rules to the prompt
+    prompt += '\nRules:\n'
+    for rule in data['rules']:
+      prompt += f'- {rule}\n'
+    
+    # Add preferences to the prompt
+    prompt += '\nPreferences:\n'
+    for preference in data['preferences']:
+      prompt += f'- {preference}\n'
+    
+    # Add the question
+    prompt += f'\nQuestion: {data['question']}\n'
+
+    return prompt
---
+++ src/turn_controller.py
@@ -0,0 +1,36 @@
+from typing import Dict
+
+from builder.data_loader import DataLoader
+from builder.prompt_generator import PromptGenerator
+
+class TurnController:
+  def __init__(self, domain_path: Dict[str, str]):
+    self.data_loader = DataLoader(domain_path)
+    self.prompt_generator = PromptGenerator()
+
+    # Load data
+    self.data = self.data_loader.get_data()
+
+    # For each domain:
+    for domain, scenarios in self.data.items():
+      # For each scenario
+      for i, scenario in enumerate(scenarios):
+        prompt = self.prompt_generator.generate_prompt(scenario)
+        
+        # Run base prompt
+
+        output = 'Test model base output'
+        self.prompt_generator.create_history(prompt, output)
+
+        # For each update
+        for j, update in enumerate(scenario['updates']):
+          prompt = self.prompt_generator.generate_prompt(update, is_update=True)
+          
+          # Run update prompt
+
+          output = 'Test model updated output'          
+          self.prompt_generator.update_history(prompt, output)
+
+      self.prompt_generator.delete_history()
+      # Write to file
+      # Run evaluation on outputs file
