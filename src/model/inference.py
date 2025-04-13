28a6254d7cb654427cab551aa60bb56974138fc8
Jayant Dabas <jayantd1112@gmail.com>
2025-04-13 02:13:25 -0400

    add chat and sentiment

--- src/model/inference.py
+++ src/model/inference.py
@@ -1,17 +1,49 @@
-from transformers import pipeline
-from transformers import Pipeline, AutoTokenizer, AutoModelForCausalLM
+import os
+
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+import torch
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 class ModelInference:
   def __init__(self, model='meta-llama/Llama-3.2-1B'):
     self.tokenizer = AutoTokenizer.from_pretrained(model)
-    self.model = AutoModelForCausalLM.from_pretrained(model)
-    # self.tokenizer = AutoTokenizer.from_pretrained('meta-llama/Llama-3.2-3B')
-    # self.model = AutoModelForCausalLM.from_pretrained('meta-llama/Llama-3.2-3B')
-    self.mode.eval()
+    
+    self.model = AutoModelForCausalLM.from_pretrained(model, token=os.environ.get('HF_TOKEN'))
+    self.model.eval()
 
-  def chat(self, message):
-    inputs = self.tokenizer(message, return_tensors='pt')
-    # Generate
-    generate_ids = self.model.generate(inputs.input_ids)
-    return self.tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+    if self.tokenizer.pad_token is None:
+      self.tokenizer.pad_token = self.tokenizer.eos_token
+
+    # Use GPU if it's available
+    self.device = 0 if torch.cuda.is_available() or torch.backends.mps.is_available() else None
+    self.model.to(self.device)
+
+  def chat(self, message, max_new_tokens=5):
+    inputs = self.tokenizer(message, return_tensors='pt').to(self.device)
+    outputs = self.model.generate(
+      **inputs, 
+      max_new_tokens=max_new_tokens,
+      pad_token_id=self.tokenizer.eos_token_id
+    )
+    response = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return response[len(message):].strip().lower()
+  
+  def get_sentiment(self, text: str) -> int:
+    try:
+      nltk.data.find('sentiment/vader_lexicon.zip')
+    except LookupError:
+      nltk.download('vader_lexicon')
+    text = text.lower().strip()
+    sid = SentimentIntensityAnalyzer()
+    scores = sid.polarity_scores(text)
+
+    if scores['pos'] > scores['neg']:
+      return 1
+    elif scores['pos'] < scores['neg']:
+      return 0
+    else:
+      return 1 if scores['compound'] >= 0.05 else 0
     
